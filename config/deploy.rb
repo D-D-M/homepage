43dd7605c0025cbbaab3fcc7a69f445b18c91a96
David Meyer <meyerdave4@gmail.com>
2018-02-24 23:24:35 -0800

    Update deploy task with recommendations based on https://medium.com/pixelpoint/deploy-rails-application-with-mina-nginx-and-puma-96a9caac9078

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,8 +50,10 @@ task :deploy do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
+    comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
+    invoke :'rvm:load_env_vars'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
@@ -62,6 +64,7 @@ task :deploy do
         command %{mkdir -p tmp/}
         command %{touch tmp/restart.txt}
       end
+      invoke :'puma:phased_restart'
     end
   end
 
